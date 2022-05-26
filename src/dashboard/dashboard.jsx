e3a541f30dc54dc84ee8718f363d67e897339563
andytxr <andreluiiz.gst@gmail.com>
2022-05-26 15:15:49 -0300

    Preparing the Dashboard with Widgets

---
+++ src/dashboard/dashboard.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+
+import ContentHeader from '../common/template/contentHeader';
+import Content from '../common/template/content';
+import ValueBox from "../common/widget/valueBox";
+
+class Dashboard extends Component{
+
+    constructor(props){
+
+        super(props);
+
+    }
+
+    render(){
+
+        return(
+
+            <div>
+                <ContentHeader title='Dashboard' small='Versão 1.0'></ContentHeader>
+                <Content>
+                    <ValueBox cols='12 4' color='green' icon='bank' value='R$10' text='Total de Créditos'></ValueBox>
+                </Content>
+            </div>
+
+        )
+
+    }
+
+}
+
+export default Dashboard
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import '../common/template/dependencies';
 import Header from '../common/template/header';
 import Sidebar from '../common/template/sidebar';
+import Footer from '../common/template/footer';
+import Routes from './routes';
 
 export default props => {
 
@@ -11,6 +13,10 @@ export default props => {
         <div className='wrapper'>
             <Header></Header>
             <Sidebar></Sidebar>
+            <div className='content-wrapper'>
+                <Routes></Routes>
+            </div>
+            <Footer></Footer>
         </div>
 
     )
