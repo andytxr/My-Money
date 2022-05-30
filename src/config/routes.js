6c4f57c27eace588888b21a26e67eac0d25d1ffa
andytxr <andreluiiz.gst@gmail.com>
2022-05-30 14:53:05 -0300

    Creating a Auth Service

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const auth = require('./auth');
 
 module.exports = function(server){
 
-    //URL base
-    const router = express.Router()
-    server.use('/api', router);
+    //Protected Routes
 
-    //Cycle Payment
+    const protectedApi = express.Router();
+    server.use('/api', protectedApi);
+
+    protectedApi.use(auth);
 
     const BillingCycle = require('../api/billingCycle/billingCycleService');
-    BillingCycle.register(router, '/billingCycles');
+    BillingCycle.register(protectedApi, '/billingCycles');
+
+    //Unprotected Routes
+
+    const openApi = express.Router();
+    server.use('/oapi', openApi);
+
+    const AuthService = require('../api/user/authService');
+    openApi.post('/login', AuthService.login);
+    openApi.post('/signup', AuthService.signup);
+    openApi.post('/valideteToken', AuthService.validateToken);
 
 }
